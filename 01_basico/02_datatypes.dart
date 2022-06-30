ba832aa343d09bf8e45e736e038dbae986ad1bb8
hectorpuga <pugagarciah@gmail.com>
2022-06-30 16:23:35 -0500

    Tipado de numeros

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,15 @@
+void main() {
+  // ======= Números
+
+  int a = 10;
+  double b = 5.5;
+
+  int? c = null;
+
+  int _a = 30;
+  double $b = 40;
+
+  double resultado = _a + $b;
+
+  print(resultado);
+}
