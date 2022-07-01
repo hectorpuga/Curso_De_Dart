4fcd7ff65f39260d236460b019b9c1ad15e8d015
hectorpuga <pugagarciah@gmail.com>
2022-07-01 03:50:19 -0500

    variables tipo const y final

---
+++ 01_basico/03_finalconst.dart
@@ -0,0 +1,20 @@
+void main() {
+  var a = 10;
+  final double b = 10.0;
+  const double c = 10.0;
+
+  // a = 20;
+  // b = 20;
+  // c = 20;
+
+  // final personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  // const personasConst = ['Juan', 'Pedro', 'Fernando'];
+
+  final List<String> personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  List<String> personasConst = const ['Juan', 'Pedro', 'Fernando'];
+
+  personasFinal.add('María');
+  // personasConst.add('María');
+
+  // print(personasFinal);
+}
