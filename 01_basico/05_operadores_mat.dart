880a7cb9caca056414c348e009759b07e268817a
hectorpuga <pugagarciah@gmail.com>
2022-07-02 02:15:04 -0500

    Operadores aritméticos

---
+++ 01_basico/05_operadores_mat.dart
@@ -0,0 +1,25 @@
+/**
+ * Un operador es un símbolo que le dice al compilador
+ * qué debe de realizar una tarea
+ * matemática, relacional o lógica
+ * y debe de producir un resultado
+ */
+void main() {
+  int a = 10 + 5; // + 15
+  a = 20 - 10; // - 10
+  a = 10 * 2; // * 20
+
+  double b = 10 / 2; // / 5
+  b = 10.0 % 3; // % 1 el sobrante de la división
+  b = -b; // -expresión Es usado para cambiar el valor de la expresión
+
+  int c = 10 ~/ 3; // ~/ 3 División común y corriente
+
+  int d = 1;
+
+  d++; // ++ 2
+  d--; // -- 1
+
+  d += 2; // += 3
+  d -= 2; // -= 1 *=  /=
+}
