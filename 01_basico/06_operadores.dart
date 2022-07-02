e4c946a233c03c0642393f0055943e5fee9fc061
hectorpuga <pugagarciah@gmail.com>
2022-07-02 15:43:37 -0500

    Operadores de asignación, condicional, relacional y de tipo

---
+++ 01_basico/06_operadores.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Operadores de asignación
+  int? a = 10;
+  int? b = 1;
+
+// Asignar el valor únicamente si la variable es null
+
+  //b ??= 20;
+  // print(b);
+
+// Operadores condicionales
+
+  int c = 26;
+
+  String resp = c > 25 ? 'C es mayor a 25' : "C es menor a 25";
+
+  // print(resp);
+
+  // int? d = b ?? a;
+
+  // print(d);
+}
