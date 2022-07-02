de6f10935cd0926fde4c82ed8e3b7aacf8713ec0
hectorpuga <pugagarciah@gmail.com>
2022-07-02 15:58:21 -0500

    Operadores relacionales y por tipo

--- 01_basico/06_operadores.dart
+++ 01_basico/06_operadores.dart
@@ -19,4 +19,39 @@ void main() {
   // int? d = b ?? a;
 
   // print(d);
+
+  // Operadores Relacionales
+  // Todos retornan un valor booleano
+  /*
+  > Mayor que
+  < Menor que
+  >= Mayor o igual que
+  <= Menor o igual que
+
+  == Revisa si dos objetos son iguales
+  != Revisa si dos objetos son diferentes
+
+  */
+
+  String persona1 = 'Fernando';
+  String persona2 = 'Alberto';
+
+  // print(persona1 == persona2);
+  // print(persona1 != persona2);
+
+  int x = 20;
+  int y = 30;
+
+  // print(x > y); //false
+  // print(x < y); //true
+  // print(x >= y); //false
+  // print(x <= y); // true
+
+  //Operador de tipo
+
+  int i = 10;
+  String j = '10';
+
+  print(i is int);
+  print(j is! int);
 }
