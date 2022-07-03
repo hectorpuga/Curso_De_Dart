d2d057ce4716da473bb6bfb6a0936de26852d7a3
hectorpuga <pugagarciah@gmail.com>
2022-07-03 18:00:54 -0500

    Argumentos a las funciones por nombre

--- 03_funciones/02_argumentos.dart
+++ 03_funciones/02_argumentos.dart
@@ -3,6 +3,11 @@ void saludar(String mensaje,
   print('$mensaje $nombre - $edad');
 }
 
+void saludar2(String mensaje, {required String nombre, int veces = 10}) {
+  print('Saludar2: $mensaje $nombre - $veces');
+}
+
 void main() {
   saludar("Hola", "Héctor", 20);
+  saludar2("Saludos", nombre: "Alejandro", veces: 20);
 }
