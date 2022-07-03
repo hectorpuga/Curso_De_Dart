52fe92561559832b3bb220491e36a7142501bcbd
hectorpuga <pugagarciah@gmail.com>
2022-07-03 14:58:45 -0500

    Ciclo For con etiquetas

---
+++ 02_control/08_labelfor.dart
@@ -0,0 +1,13 @@
+void main() {
+  outrLoop:
+  for (int i = 0; i < 5; i++) {
+    print('Valor de i: $i');
+    innerLoop:
+    for (int j = 0; j < 5; j++) {
+      print('Valor de j: $j');
+      if (j == 2) {
+        break outrLoop;
+      }
+    }
+  }
+}
