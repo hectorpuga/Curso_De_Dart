b5471b06e6d9141ca2009d1177f79f51b46bc3ef
hectorpuga <pugagarciah@gmail.com>
2022-07-03 20:36:24 -0500

    Tarea sobre funciones

--- 03_funciones/06_tarea_funciones.dart
+++ 03_funciones/06_tarea_funciones.dart
@@ -1,82 +1,38 @@
-/** 
- *  Dado el siguiente código 
- *    Optimizarlo lo más posible usando funciones
- *    y todo lo visto en el curso
- * 
- *  Ver el primer ejemplo:
-*/
 import 'dart:io';
 
-main() {
-
-  // Ejemplo: 
-  // Crear una función para imprimir STDOUTS en lugar de 
-  // la siguiente línea. Luego todos los stdouts deberían ser
-  // llamados usando nuestra función personalizada
-  stdout.writeln('=========== Usuario 1 =============');
-
-
-  stdout.writeln('¿Cúal es su nombre?');
-  String nombre = stdin.readLineSync() ?? '';
-
-  stdout.writeln('¿Qué edad tienes?');
-  String edad = stdin.readLineSync() ?? '';
-  
-  stdout.writeln('¿En qué país naciste?');
-  String pais = stdin.readLineSync() ?? '';
-  
-
-  final Map<String, dynamic> usuario = {
-    'nombre': nombre,
-    'edad'  : edad,
-    'pais'  : pais
-  };
-
-  stdout.writeln('Usuario 1 sin deducciones');
-  stdout.writeln( usuario );
-
-  double salario     = 1500;
-  double deducciones = salario * 0.15;
-  double salarioNeto = salario - deducciones;
-
-  usuario['salario']     = salario;
-  usuario['deducciones'] = deducciones;
-  usuario['salarioNeto'] = salarioNeto;
-
-  stdout.writeln(usuario);
-
-
-  // Persona 2
-  stdout.writeln('=========== Usuario 2 =============');
-
-  stdout.writeln('¿Cúal es su nombre?');
-  String nombre2 = stdin.readLineSync() ?? '';
-
-  stdout.writeln('¿Qué edad tienes?');
-  String edad2 = stdin.readLineSync() ?? '';
-  
-  stdout.writeln('¿En qué país naciste?');
-  String pais2 = stdin.readLineSync() ?? '';
-  
-
-  final Map<String, dynamic> usuario2 = {
-    'nombre': nombre2,
-    'edad'  : edad2,
-    'pais'  : pais2
-  };
-
-  stdout.writeln('Usuario 2 sin deducciones');
-  stdout.writeln( usuario2 );
-
-  double salario2     = 1800;
-  double deducciones2 = salario2 * 0.15;
-  double salarioNeto2 = salario2 - deducciones2;
-
-  usuario2['salario']     = salario2;
-  usuario2['deducciones'] = deducciones2;
-  usuario2['salarioNeto'] = salarioNeto2;
-
-  stdout.writeln(usuario2);
-
+String input(String texto, {verdad = true}) {
+  stdout.writeln(texto);
+  return verdad ? stdin.readLineSync() ?? '' : "";
 }
 
+Map<String, dynamic> datos(
+        double salario, double deducciones, double salarioNeto) =>
+    {
+      'nombre': input('¿Cúal es su nombre?'),
+      'edad': input('¿Que edad tienes?'),
+      'pais': input('¿En qué país naciste?'),
+      "salario": salario,
+      "deducciones": deducciones,
+      "salarioNeto": salarioNeto
+    };
+main() {
+  input('=========== Usuario 1 =============', verdad: false);
+  double salario = 1500;
+  double deducciones = salario * 0.15;
+  double salarioNeto = salario - deducciones;
+  final Map<String, dynamic> usuario = datos(salario, deducciones, salarioNeto);
+  input('Usuario 1 sin deducciones', verdad: false);
+  input(usuario.toString(), verdad: false);
+
+  // Persona 2
+  input('=========== Usuario 2 =============', verdad: false);
+
+  salario = 1800;
+  deducciones = salario * 0.15;
+  salarioNeto = salario - deducciones;
+
+  final Map<String, dynamic> usuario2 =
+      datos(salario, deducciones, salarioNeto);
+  stdout.writeln('Usuario 2 sin deducciones');
+  stdout.writeln(usuario2);
+}
