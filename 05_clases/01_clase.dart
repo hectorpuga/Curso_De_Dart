efc4e532da776b4cb7849e737f90dca53e34e11a
hectorpuga <pugagarciah@gmail.com>
2022-07-06 14:19:36 -0500

    Clases en archivos independientes

--- 05_clases/01_clase.dart
+++ 05_clases/01_clase.dart
@@ -1,35 +1,12 @@
+import 'clases/persona.dart';
+
 void main() {
-  // final persona = {
-  //   "nombre": "Hector",
-  //   "edad": 33,
-  //   "bio": "Nació en Guadalajara"
-  // };
-
-  // print(persona['edad']);
-
   final persona = new Persona();
 
-  persona.nombre = 'Fernando';
-  persona.edad = 33;
-  persona.bio = 'Nació por ahi';
+  persona
+    ..nombre = 'Fernando'
+    ..edad = 33
+    ..bio = "Nació por ahi";
 
-  print(persona.toString());
-}
-
-class Persona {
-  // Campos o propiedades
-
-  String? nombre;
-  int? edad;
-  String? bio;
-
-  // Get y Sets
-
-  // Contructores
-
-  // métodos
-  @override
-  String toString() {
-    return '$nombre $edad $bio';
-  }
+  print(persona);
 }
---
+++ 05_clases/clases/persona.dart
@@ -0,0 +1,15 @@
+class Persona {
+  // Campos o propiedades
+
+  String? nombre;
+  int? edad;
+  String? bio;
+
+  // Get y Sets
+
+  // Contructores
+
+  // métodos
+  @override
+  String toString() => '$nombre $edad $bio';
+}
