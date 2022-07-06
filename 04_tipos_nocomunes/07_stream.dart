9e5edb557f92294da5b2416f088cfb65d68a559c
hectorpuga <pugagarciah@gmail.com>
2022-07-06 13:26:46 -0500

    Tipado y Broadcast

--- 04_tipos_nocomunes/07_stream.dart
+++ 04_tipos_nocomunes/07_stream.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
 
 void main() {
-  final streamController = StreamController();
+  final streamController = new StreamController<String>.broadcast();
   streamController.stream.listen((data) => print('despegando! $data'),
       onError: (error) => print('Error! $error'),
       cancelOnError: false,
       onDone: () => print('Misión completa!'));
+
+  streamController.stream.listen((data) => print('despegando Stream 2! $data'),
+      onError: (error) => print('Error Stream 2! $error'),
+      cancelOnError: false,
+      onDone: () => print('Misión completa! Stream 2'));
 
   streamController.sink.add('Apollo 11');
   streamController.sink.add('Apollo 12');
