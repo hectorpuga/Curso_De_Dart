8106d3f8170ac9df0da447a9889d09a89e42bc96
hectorpuga <pugagarciah@gmail.com>
2022-07-08 02:35:19 -0500

    Super constructor

---
+++ 06_clases_adv/02_abstractas.dart
@@ -0,0 +1,32 @@
+abstract class Vehiculo {
+  bool encendido = false;
+
+  void encender() {
+    encendido = true;
+    print('Vehiculo encendido');
+  }
+
+  void apagar() {
+    encendido = false;
+    print('Vehiculo apagado');
+  }
+
+  bool revisarMotor();
+}
+
+class Carro extends Vehiculo {
+  int kilometraje = 0;
+
+  @override
+  bool revisarMotor() {
+    print('Motor ok');
+    return true;
+  }
+}
+
+void main() {
+  final ford = new Carro();
+  ford.encender();
+  ford.apagar();
+  ford.revisarMotor();
+}
