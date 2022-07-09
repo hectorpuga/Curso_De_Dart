4b7f31ed932150606d1052c2a98da6fa66b95ad6
hectorpuga <pugagarciah@gmail.com>
2022-07-09 14:12:55 -0500

    Mapas: Propiedades y métodos

---
+++ 07_docs/04_maps.dart
@@ -0,0 +1,27 @@
+void main() {
+  final persona = {'nombre': 'Fernando', 'apellido': 'Herrera', 'edad': 33};
+  final direccion = {'ciudad': 'Ottawa', 'pais': 'Canadá'};
+
+  print('Persona : $persona');
+  print('Length: ${persona.length}');
+  print('Keys: ${persona.keys}');
+  print('Values ${persona.values}');
+
+  persona.addAll(direccion);
+  print('AddAll: $persona');
+
+  persona.remove('pais');
+  print('Remove: $persona');
+
+  // persona.removeWhere((key, value) {
+  //   if (key != 'nombre') {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
+
+  persona.removeWhere((key, value) => key != 'nombre');
+
+  print('RemoveWhere: $persona');
+}
