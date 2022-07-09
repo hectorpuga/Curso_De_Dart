a944967ec685dc96ac524bc5bcfbb88762a4c8d7
hectorpuga <pugagarciah@gmail.com>
2022-07-09 13:25:09 -0500

    List: Más métodos

--- 07_docs/03_list.dart
+++ 07_docs/03_list.dart
@@ -16,6 +16,37 @@ void main() {
   print('ListaMapa ${listaMapa[4]}');
   Map nombreMapa = nombres.asMap();
   print(nombreMapa);
-
+  print('NombreMapa: ${nombreMapa}');
   print('NombreMapa: ${nombreMapa[1]}');
+
+  print('IndexOf: ${nombres.indexOf('Peter')}');
+
+  // int mayor3 = lista.indexWhere((numero) {
+  //   if (numero > 3) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
+
+  int mayor3 = lista.indexWhere((numero) => numero > 3);
+
+  print('indexWhere mayor 3: $mayor3');
+
+  print('Remove: ${nombres.remove('Tony')}');
+  print('Remove: ${nombres}');
+
+  lista.shuffle();
+  print('Shuffle: $lista');
+  lista3.sort();
+  print('Sort: $lista3');
+  print('Reverse: ${lista3.reversed.toList()}');
+
+  nombres.forEach((nombre) => print(nombre.toUpperCase()));
+
+  print('Listado: $nombres');
+
+  final newList = nombres.map((nombre) => nombre.toUpperCase()).toList();
+
+  print('newList: $newList');
 }
